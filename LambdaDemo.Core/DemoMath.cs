11ad820af9f526530a6e8e7f93f99cf53359ff74
Wijitha Wijenayake <wijitha@calcey.com>
2020-08-18 17:53:00 +0530

    Base project to start the demo

---
+++ LambdaDemo.Core/DemoMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LambdaDemo.Core
+{
+    public static class DemoMath
+    {
+        public static int FibDelayed(int n)
+        {
+            Task.Delay(2 * 1000).Wait();
+            return Fib(n);
+        }
+
+        public static int Fib(int n)
+        {
+            if (n == 0)
+                return 0;
+
+            if (n == 1)
+                return 1;
+
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
---
+++ LambdaDemo.Main/Program.cs
@@ -0,0 +1,36 @@
+﻿using LambdaDemo.Core;
+using System;
+
+namespace LambdaDemo.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var upto = 10;
+
+            if (args.Length > 0)
+                upto = int.Parse(args[0]);
+
+            Console.WriteLine($"Starting finding Fibs upto {upto}...");
+
+            var startTime = DateTime.Now;
+
+            for (var i = 0; i <= upto; i++)
+            {
+                var fib = DemoMath.FibDelayed(i);
+                Console.WriteLine($"Fib({i}): {fib}");
+            }
+
+            var endTime = DateTime.Now;
+
+            Console.WriteLine("Done!");
+
+            var elapsedTime = endTime - startTime;
+
+            Console.WriteLine($"Elapsed Time: {elapsedTime.Minutes} Mins {elapsedTime.Seconds} Secs");
+
+            Console.ReadKey();
+        }
+    }
+}
